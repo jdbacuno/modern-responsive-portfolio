8de6d6a6658977f2c872800be09caa4a024e28b8
jdbacuno <jonas.d.bacuno@gmail.com>
2026-02-16 11:57:23 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Internship Task: Modern Reponsive Porfolio Website
+
+## Objective
+
+Develop a modern, responsive portfolio website to showcase professional skills, projects, and achievements.
---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('open');
+  mobileMenu.classList.toggle('open');
+});
+
+function closeMobile() {
+  hamburger.classList.remove('open');
+  mobileMenu.classList.remove('open');
+}
